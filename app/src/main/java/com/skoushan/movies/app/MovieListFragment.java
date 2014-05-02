6e8c2a6042040c70b43a712d096171af8acac8ae
Soheil Koushan <soheil.koushan@gmail.com>
2014-05-02 14:28:38 -0400

    deleted unnecessary commented code

--- app/src/main/java/com/skoushan/movies/app/MovieListFragment.java
+++ app/src/main/java/com/skoushan/movies/app/MovieListFragment.java
@@ -12,7 +12,6 @@ import java.util.Map;
 /**
  * Created by dx166-xl on 2014-05-01.
  */
-// TODO TURN TO LIST FRAGMENT!!!
 //http://developer.android.com/training/implementing-navigation/lateral.html
 public class MovieListFragment extends ListFragment {
 
@@ -20,15 +19,6 @@ public class MovieListFragment extends ListFragment {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
     }
-
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//        //View rootView = inflater.inflate (R.layout.fragment_movielist, container, false);
-//
-//        // TODO http://developer.android.com/guide/components/activities.html#SavingActivityState
-//        //ListView lv = (ListView) rootView.findViewById(R.id.listView);
-//        return rootView;
-//    }
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id){
